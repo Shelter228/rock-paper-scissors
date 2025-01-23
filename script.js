2c8ca989f6273f7be41f14770fd728798e42af04
Islam <baydulaev.95@mail.ru>
2025-01-23 19:37:31 +0300

    first part

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-function getComputerChoice ()  {
-
-let choice =  Math.floor(Math.random() * 3);
-if  (choice === 0) {
-    return "камень"
-}
-else if (choice === 1) {
-    return "ножницы"
-}
-else if (choice === 2) {
-    return "бумага"
-}
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "камень";
+  } else if (choice === 1) {
+    return "ножницы";
+  } else if (choice === 2) {
+    return "бумага";
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    let compChoice = computerSelection.toLowerCase();
-    let playerText = playerSelection.toLowerCase();
+  let compChoice = computerSelection.toLowerCase();
+  let playerText = playerSelection.toLowerCase();
+
+  if (playerText === compChoice) {
+    return `Ничья! Выбор соперника: ${compChoice}`;
+  } else if (
+    (playerText === "камень" && compChoice === "ножницы") ||
+    (playerText === "ножницы" && compChoice === "бумага") ||
+    (playerText === "бумага" && compChoice === "камень")
+  ) {
+    return `Вы выиграли! Выбор соперника: ${compChoice}`;
+  } else if (
+    (compChoice === "камень" && playerText === "ножницы") ||
+    (compChoice === "ножницы" && playerText === "бумага") ||
+    (compChoice === "бумага" && playerText === "камень")
+  ) {
+    return `Вы проиграли! Выбор соперника: ${compChoice}`;
+  }
+  else {
+    return " Неверно, Вы должны ввести: Камень Ножницы или Бумага"
+  }
+};
 
 
-    if (playerText === compChoice) {
-      return ("ничья");
-    }
-
-    else if  (
-        (playerText === "камень" && compChoice === "ножницы") ||
-        (playerText === "ножницы" && compChoice === "бумага") ||
-        (playerText === "бумага" && compChoice === "камень")
-    ) {
-        return ("вы выиграли");
-    }
-    else {
-        return ("вы проиграли");
-    }
- }
- let playerSelection = prompt("выберите : камень ножницы или бумага");
+let playerSelection = prompt("выберите : камень ножницы или бумага");
 let computerSelection = getComputerChoice();
-console.log("Выбор игрока:", playerSelection);
-console.log("Выбор компьютера:", computerSelection);
 let result = playRound(playerSelection, computerSelection);
 alert(result);
